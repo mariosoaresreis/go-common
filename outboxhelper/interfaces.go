a74d0e4568d2b78f28d23101d8564f3342ff1ca6
Mario Reis <mario.reis@optiisolutions.com>
2024-09-03 20:19:16 -0300

    interfaces

--- outboxhelper/interfaces.go
+++ outboxhelper/interfaces.go
@@ -1,1 +1,11 @@
 package outboxhelper
+
+import "github.com/mariosoaresreis/go-common/datamodels"
+
+type IdTypes interface {
+	string | int64
+}
+
+type OutboxTypes interface {
+	GetOutboxTypeInfo() (typ datamodels.AggregateType, version string)
+}
